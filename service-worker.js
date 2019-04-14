1169121a37af0ffb366b11c4cdfbd00b880aeea8
Viktor <vnrazumov@gmail.com>
2019-04-14 21:01:42 +0200

    Setup prod version of workbox

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,8 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js')
 
 if (workbox) {
+  workbox.setConfig({ debug: false })
+
   workbox.precaching.precacheAndRoute([
     'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.slim.js',
     'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js',
